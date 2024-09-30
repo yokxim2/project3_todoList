c4f180db9fc0134d1b73c08c073c72bbabad1d04
yokxim <yooksi53@gmail.com>
2024-09-30 15:18:51 +0900

    Todo class + TodoController fixed

--- src/main/java/com/sparta/project3_todolist/controller/TodoController.java
+++ src/main/java/com/sparta/project3_todolist/controller/TodoController.java
@@ -1,17 +1,38 @@
 package com.sparta.project3_todolist.controller;
 
+import com.sparta.project3_todolist.dto.TodoRequestDto;
 import com.sparta.project3_todolist.dto.TodoResponseDto;
+import com.sparta.project3_todolist.entity.Todo;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api")
 public class TodoController {
 
+    private final Map<Long, Todo> todoList = new HashMap<>();
+
     @PostMapping("/todos")
     public TodoResponseDto createTodo(@RequestBody TodoRequestDto requestDto) {
-        return null;
+        // RequestDto -> Entity(Todo)
+        Todo todo = new Todo(requestDto);
+
+        // Todo Max ID Check
+        Long maxId = todoList.size() > 0 ? Collections.max(todoList.keySet()) + 1 : 1;
+        todo.setId(maxId);
+
+        // DB 저장
+        todoList.put(todo.getId(), todo);
+
+        // Entity -> ResponseDto
+        TodoResponseDto todoResponseDto = new TodoResponseDto(todo);
+
+        return todoResponseDto;
     }
 }
--- src/main/java/com/sparta/project3_todolist/entity/Todo.java
+++ src/main/java/com/sparta/project3_todolist/entity/Todo.java
@@ -1,5 +1,6 @@
 package com.sparta.project3_todolist.entity;
 
+import com.sparta.project3_todolist.dto.TodoRequestDto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,4 +16,9 @@ public class Todo {
     private String content;
     private LocalDateTime createdAt;
     private LocalDateTime modifiedat;
+
+    public Todo(TodoRequestDto requestDto) {
+        this.title = requestDto.getTitle();
+        this.content = requestDto.getContent();
+    }
 }
