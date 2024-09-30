d7e11243f48b30464099d0fa09f73c85bc31bb0f
yokxim <yooksi53@gmail.com>
2024-09-30 16:37:39 +0900

    added methods frame (POST, GET, PUT, DELETE)

--- src/main/java/com/sparta/project3_todolist/controller/TodoController.java
+++ src/main/java/com/sparta/project3_todolist/controller/TodoController.java
@@ -3,13 +3,11 @@ package com.sparta.project3_todolist.controller;
 import com.sparta.project3_todolist.dto.TodoRequestDto;
 import com.sparta.project3_todolist.dto.TodoResponseDto;
 import com.sparta.project3_todolist.entity.Todo;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -34,5 +32,39 @@ public class TodoController {
         TodoResponseDto todoResponseDto = new TodoResponseDto(todo);
 
         return todoResponseDto;
+    }
+
+    @GetMapping("/todos")
+    public List<TodoResponseDto> getTodos() {
+        // Map to List
+        List<TodoResponseDto> responseList = todoList.values().stream().map(TodoResponseDto::new).toList();
+        return responseList;
+    }
+
+    @PutMapping("/todos/{id}")
+    public Long updateTodo(@PathVariable Long id, @RequestBody TodoRequestDto requestDto) {
+        // 해당 todo가 DB에 존재하는지 확인
+        if (todoList.containsKey(id)) {
+            // 해당 todo 가져오기
+            Todo todo = todoList.get(id);
+
+            // todo 수정
+            todo.update(requestDto);
+            return todo.getId();
+        } else {
+            throw new IllegalArgumentException("선택한 일정은 존재하지 않습니다.");
+        }
+    }
+
+    @DeleteMapping("/todos/{id}")
+    public Long deleteTodo(@PathVariable Long id) {
+        // 해당 todo가 DB에 존재하는지 확인
+        if (todoList.containsKey(id)) {
+            // 해당 todo 삭제
+            todoList.remove(id);
+            return id;
+        } else {
+            throw new IllegalArgumentException("선택한 일정은 존재하지 않습니다.");
+        }
     }
 }
