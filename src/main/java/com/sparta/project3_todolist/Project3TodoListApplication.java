b4c0eab9e1451ae5c511ecb28946d7a94ccadeec
yokxim <yooksi53@gmail.com>
2024-09-30 14:03:21 +0900

    Initial commit

---
+++ src/main/java/com/sparta/project3_todolist/Project3TodoListApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.project3_todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project3TodoListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project3TodoListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/project3_todolist/Project3TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.project3_todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project3TodoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
