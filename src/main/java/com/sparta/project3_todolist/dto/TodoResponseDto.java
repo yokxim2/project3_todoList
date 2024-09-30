4bfe12c930c5de4c65faba0baf7e9e44d3346b51
yokxim <yooksi53@gmail.com>
2024-09-30 18:38:42 +0900

    DateTimeFormat added

--- src/main/java/com/sparta/project3_todolist/dto/TodoResponseDto.java
+++ src/main/java/com/sparta/project3_todolist/dto/TodoResponseDto.java
@@ -12,22 +12,22 @@ public class TodoResponseDto {
     private Long id;
     private String title;
     private String content;
-    private LocalDateTime createdAt;
-    private LocalDateTime modifiedAt;
+    private String createdAt;
+    private String modifiedAt;
 
     public TodoResponseDto(Todo todo) {
         this.id = todo.getId();
         this.title = todo.getTitle();
         this.content = todo.getContent();
-        this.createdAt = todo.getCreatedAt();
-        this.modifiedAt = todo.getModifiedAt();
+        this.createdAt = todo.getFormattedCreatedAt();
+        this.modifiedAt = todo.getFormattedModifiedAt();
     }
-
-    public TodoResponseDto(Long id, String title, String content, LocalDateTime createdAt, LocalDateTime modifiedAt) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-        this.createdAt = createdAt;
-        this.modifiedAt = modifiedAt;
-    }
+//
+//    public TodoResponseDto(Long id, String title, String content, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+//        this.id = id;
+//        this.title = title;
+//        this.content = content;
+//        this.createdAt = createdAt;
+//        this.modifiedAt = modifiedAt;
+//    }
 }
