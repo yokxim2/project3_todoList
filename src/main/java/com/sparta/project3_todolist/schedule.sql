3dad5319f2c556a2a377077df0401dade48768df
yokxim <yooksi53@gmail.com>
2024-10-01 10:52:28 +0900

    two table -> one table temporarily fixed

--- src/main/java/com/sparta/project3_todolist/schedule.sql
+++ src/main/java/com/sparta/project3_todolist/schedule.sql
@@ -1,17 +1,9 @@
 create table if not exists todo (
-    id bigint primary key not null,
-    member_id bigint not null,
-    content varchar(200),
-    created_at datetime,
-    modified_at datetime
+                                    id bigint primary key not null auto_increment,
+                                    title varchar(30) not null,
+                                    content varchar(200) not null,
+                                    username varchar(30) not null,
+                                    password varchar(30) not null,
+                                    created_at datetime not null,
+                                    modified_at datetime not null
 );
-
-create table if not exists member (
-    id bigint primary key not null,
-    email varchar(30) not null,
-    password varchar(30) not null,
-    registered_at datetime,
-    modified_at datetime
-);
-
-alter table todo add constraint todo_fk_member_code foreign key(member_id) references member(id);
