3a20c5142448c443d63e99c8ccb791f7e0d1d594
yokxim <yooksi53@gmail.com>
2024-10-02 15:16:40 +0900

    switched foreign key: let member:id reference todo:memberId, instead of letting todo:memberId reference member:id

--- src/main/java/com/sparta/project3_todolist/schedule.sql
+++ src/main/java/com/sparta/project3_todolist/schedule.sql
@@ -4,14 +4,14 @@ create table if not exists member (
                                       username varchar(30) not null,
                                       password varchar(30) not null,
                                       registered_at datetime not null,
-                                      modified_at datetime not null
+                                      modified_at datetime not null,
+                                      constraint member_fk_todo foreign key (id) references todo(member_id)
 );
 create table if not exists todo (
                                     id bigint primary key not null auto_increment,
-                                    member_id bigint not null,
+                                    member_id bigint unique key not null,
                                     title varchar(30) not null,
                                     content varchar(200) not null,
                                     created_at datetime not null,
-                                    modified_at datetime not null,
-                                    constraint todo_fk_member foreign key (member_id) references member(id)
+                                    modified_at datetime not null
 );
