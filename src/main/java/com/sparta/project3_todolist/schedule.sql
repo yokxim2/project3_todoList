ee073fa16c1e5dc47bea6718a5443a41ea487097
yokxim <yooksi53@gmail.com>
2024-10-02 13:26:08 +0900

    added another table "member"

--- src/main/java/com/sparta/project3_todolist/schedule.sql
+++ src/main/java/com/sparta/project3_todolist/schedule.sql
@@ -1,9 +1,17 @@
+
+create table if not exists member (
+                                      id bigint primary key not null auto_increment,
+                                      username varchar(30) not null,
+                                      password varchar(30) not null,
+                                      registered_at datetime not null,
+                                      modified_at datetime not null
+);
 create table if not exists todo (
                                     id bigint primary key not null auto_increment,
+                                    member_id bigint not null,
                                     title varchar(30) not null,
                                     content varchar(200) not null,
-                                    username varchar(30) not null,
-                                    password varchar(30) not null,
                                     created_at datetime not null,
-                                    modified_at datetime not null
+                                    modified_at datetime not null,
+                                    constraint todo_fk_member foreign key (member_id) references member(id)
 );
