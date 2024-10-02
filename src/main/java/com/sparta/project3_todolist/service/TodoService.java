3f106d5abaccffec1311c3de092469c191ded3b0
yokxim <yooksi53@gmail.com>
2024-10-02 14:02:29 +0900

    Rebuild TodoService with Member entity

--- src/main/java/com/sparta/project3_todolist/service/TodoService.java
+++ src/main/java/com/sparta/project3_todolist/service/TodoService.java
@@ -2,7 +2,9 @@ package com.sparta.project3_todolist.service;
 
 import com.sparta.project3_todolist.dto.TodoRequestDto;
 import com.sparta.project3_todolist.dto.TodoResponseDto;
+import com.sparta.project3_todolist.entity.Member;
 import com.sparta.project3_todolist.entity.Todo;
+import com.sparta.project3_todolist.repository.MemberRepository;
 import com.sparta.project3_todolist.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,16 +19,19 @@ import java.util.List;
 public class TodoService {
 
     private final TodoRepository todoRepository;
+    private final MemberRepository memberRepository;
 
     public TodoResponseDto createTodo(TodoRequestDto requestDto) {
-        // RequestDto -> Entity(Todo)
+        // RequestDto -> Entity(Todo), Entity(Member)
         Todo todo = new Todo(requestDto);
+        Member member = new Member(requestDto);
 
         // DB 저장
-        Todo saveTodo = todoRepository.save(todo);
+        Member saveMember = memberRepository.save(member);
+        Todo saveTodo = todoRepository.save(todo, saveMember.getId());
 
         // Entity -> ResponseDto
-        TodoResponseDto todoResponseDto = new TodoResponseDto(saveTodo);
+        TodoResponseDto todoResponseDto = new TodoResponseDto(saveTodo, saveMember);
 
         return todoResponseDto;
     }
@@ -35,38 +40,41 @@ public class TodoService {
         return todoRepository.findAll(username, modifiedAt);
     }
 
-    public Long updateTodo(Long id, TodoRequestDto requestDto) {
+    public Long updateTodo(Long todoId, TodoRequestDto requestDto) {
         // 해당 todo가 DB에 존재하는지 확인
-        Todo todo = todoRepository.findById(id);
+        Todo todo = todoRepository.findById(todoId);
 
-        if (todo != null) {
-            // 비밀번호가 일치하는지 확인
-            if (todo.getPassword().equals(requestDto.getPassword())) {
-                // todo 내용 수정
-                todoRepository.update(id, requestDto);
-                return id;
-            } else {
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "비밀번호가 올바르지 않습니다.");
-            }
+        // todo의 memberId와 일치하는 member를 DB에서 조회
+        Member member = memberRepository.findById(todo.getMemberId());
+
+        // 비밀번호가 일치하는지 확인
+        if (member.getPassword().equals(requestDto.getPassword())) {
+            // todo 내용 수정 [content]
+            todoRepository.update(todoId, requestDto);
+            // member 내용 수정 [username]
+            memberRepository.update(member.getId(), requestDto);
+            return todoId;
         } else {
-            throw new IllegalArgumentException("선택한 일정은 존재하지 않습니다.");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "비밀번호가 올바르지 않습니다.");
         }
     }
 
-    public Long deleteTodo(Long id, TodoRequestDto requestDto) {
+    public Long deleteTodo(Long todoId, TodoRequestDto requestDto) {
         // 해당 todo가 DB에 존재하는지 확인
-        Todo todo = todoRepository.findById(id);
-        if (todo != null) {
-            // 비밀번호가 일치하는지 확인
-            if (todo.getPassword().equals(requestDto.getPassword())) {
-                // todo 삭제
-                todoRepository.delete(id);
-                return id;
-            } else {
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "비밀번호가 올바르지 않습니다.");
-            }
+        Todo todo = todoRepository.findById(todoId);
+
+        // 해당 member가 DB에 존재하는지 확인
+        Member member = memberRepository.findById(todo.getMemberId());
+
+        // 비밀번호가 일치하는지 확인
+        if (member.getPassword().equals(requestDto.getPassword())) {
+            // todo 삭제
+            todoRepository.delete(todoId);
+            // member 삭제
+            memberRepository.delete(member.getId());
+            return todoId;
         } else {
-            throw new IllegalArgumentException("선택한 일정은 존재하지 않습니다.");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "비밀번호가 올바르지 않습니다.");
         }
     }
 }
