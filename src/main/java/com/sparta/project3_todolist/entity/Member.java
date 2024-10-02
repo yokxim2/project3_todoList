d8b042c1cb1b377cb52df08e37cc2b9b79ea8652
yokxim <yooksi53@gmail.com>
2024-10-02 13:27:14 +0900

    modified member entity class

--- src/main/java/com/sparta/project3_todolist/entity/Member.java
+++ src/main/java/com/sparta/project3_todolist/entity/Member.java
@@ -1,8 +1,16 @@
 package com.sparta.project3_todolist.entity;
 
+import com.sparta.project3_todolist.dto.TodoRequestDto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class Member {
     private Long id;
     private String username;
@@ -11,4 +19,19 @@ public class Member {
     private LocalDateTime modifiedAt;
 
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    public Member(TodoRequestDto requestDto) {
+        this.username = requestDto.getUsername();
+        this.password = requestDto.getPassword();
+        this.registeredAt = LocalDateTime.now();
+        this.modifiedAt = LocalDateTime.now();
+    }
+
+    public String getFormattedRegisteredAt() {
+        return registeredAt.format(formatter);
+    }
+
+    public String getFormattedModifiedAt() {
+        return modifiedAt.format(formatter);
+    }
 }
